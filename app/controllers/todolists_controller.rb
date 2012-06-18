c55385fddd33e8db5f42b8c68b95660dbfa0fe50
Dm Dv <dimos-d@yandex.ru>
2012-06-19 03:56:06 +0400

    Refactoring

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,18 +13,22 @@ class TodolistsController < ApplicationController
   def index
     options[:caption] = 'All Todo lists'
     options[:projectid] = params[:id]
+
     options[:todolists] = lib.get_all(params[:id])
   end
 
   def index_completed
     options[:caption] = 'Completed Todo lists'
     options[:projectid] = params[:id]
+
     options[:todolists] = lib.get_completed(params[:id])
     render :template => 'todolists/index'
   end
 
   def items
+    options[:caption] = 'Todolist items'
     options[:projectid] = params[:id]
+
     todolist = lib.get_items(params[:id], params[:todoid])
     options[:todolist] = todolist
     options[:remaining] = todolist['todos']['remaining']
@@ -37,6 +41,11 @@ class TodolistsController < ApplicationController
     options[:description] = "enter description"
   end
 
+  def create
+    options[:projectid] = params[:projectid]
+    options[:todolist] = lib.create(params[:name], params[:description], params[:projectid])
+  end
+
   def new_item
     options[:projectid] = params[:id]
     options[:people] = Peoples.new.get_all.collect {|p| [ p["name"], p["id"] ] }
@@ -44,11 +53,6 @@ class TodolistsController < ApplicationController
     # possible way to create in view.
     # Peoples.new.get_all {|p| [ p.name, p.id ] }
     puts ""
-  end
-
-  def create
-    options[:projectid] = params[:projectid]
-    options[:todolist] = lib.create(params[:name], params[:description], params[:projectid])
   end
 
   def create_item
@@ -60,7 +64,6 @@ class TodolistsController < ApplicationController
     pars[:assignee_type]  = "Person"
 
     Todos.new.create(pars, params[:projectid], params[:todolistid])
-
   end
 
   def update
@@ -68,5 +71,4 @@ class TodolistsController < ApplicationController
 
   def delete
   end
-
 end
